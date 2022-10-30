8b83feddffd372069802570e9e1b5a741168ff04
RyanArif <ryan.arif2000@gmail.com>
2022-10-30 14:59:58 -0500

    Started Unit 2 Assignment
    Started with the first step, vectors

---
+++ assignments/unit2/C_STL_Library_Arif/main.cpp
@@ -0,0 +1,135 @@
+//Dr_T Unit 2 COSC-1437 Starter 
+/* Task -
+
+Top level solution folder: C_STL_Library_Arif
+
+create a single main.cpp that contains code samples and implementations of each of the following topics */
+
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <string>
+using namespace std; 
+
+//Credit https://www.geeksforgeeks.org/passing-vector-constructor-c/ clarifications added
+class MyClassVector1 
+{ 
+  private:  
+      vector<int> vec; 
+  public: 
+      MyClassVector1 (vector<int> v)  //parameterized constructor
+      { 
+        vec = v; 
+      } 
+      void print() 
+      { 
+          /// print the value of vector 
+          for (int i = 0; i < vec.size(); i++) 
+          {
+              cout << vec[i] << " "; 
+          }    
+      } 
+};
+
+int main() 
+{
+    /****2022FL COSC-1437-58001***Vectors*/ 
+    //Write the base code for: 5. std::vector example
+    vector<double> vd; //vd elments are floating point numbers
+    vector<int> vi; //vi elements are integer numbers 
+    vector<string> vs; //vs elements are string objects 
+
+    //Expand this code to:
+
+    //add 3 elements to the vd vector
+    vd.push_back(1);
+    vd.push_back(2);
+    vd.push_back(3);
+    vd.push_back(55.4); 
+
+    //add 3 elements to the vi vector
+    vi.push_back(2);
+    vi.push_back(42);
+    vi.push_back(300);
+    // add 3 elements to the vs vector
+    vs.push_back("string1");
+    vs.push_back("string2");
+    vs.push_back("string3");
+
+   // display the 3 elements in the vd vector
+   cout << "\nValues in vd: \n"; 
+   for(double vals : vd)
+   {
+     cout << vals << endl; 
+   }
+   cout << "\nAnother way to print vector: " << endl; 
+   for(int i = 0; i < vd.size(); i++)
+   {
+     cout << vd[i] << endl; 
+   }
+
+    //display the 3 elements in the vi vector
+   cout << "\nValues in vi: \n"; 
+   for(int vals : vi)
+   {
+     cout << vals << endl; 
+   }
+   // display the 3 elements in the vs vector
+   cout << "\nValues in vs: \n"; 
+   for(string vals : vs)
+   {
+     cout << vals << endl; 
+   }
+   
+    /****2022FL COSC-1437-58001***Vector_as_Class_Member*/ 
+    //Credit https://www.geeksforgeeks.org/passing-vector-constructor-c/
+
+   cout << "\nVector_as_Class_Member" << endl; 
+    vector<int> vec; 
+        for (int i = 1; i <= 5; i++) 
+            vec.push_back(i); 
+        MyClassVector1 obj(vec); 
+        obj.print(); 
+      
+      //Continue with MyClassVector2 and MyClassVector3
+
+    /****2022FL COSC-1437-58001***STL_Iterators*/ 
+    
+
+    /****2022FL COSC-1437-58001*** Stack*/
+
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001**** Set*/
+    //Write the code as presented in: 3. std::set
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001****Pair_Structure*/
+    //Write the code as presented in: 4. std::pair structure
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001**** Map_Insert*/
+    //Write the code as presented in: 14. std::map::insert
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001****Map_Summary*/
+    //Write the code as presented in: 16. Map summary
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001**** Sort_Algorithm*/
+    //Write the code as presented in: 23. sort example
+
+    //Write comments that help one better understand what the code is doing.
+
+    /****2022FL COSC-1437-58001****Predicate_Algorithm*/
+    //Write the code as presented in: 25. count_if and predicate function
+
+    //Write comments that help one better understand what the code is doing. 
+
+      return 0; 
+ }
