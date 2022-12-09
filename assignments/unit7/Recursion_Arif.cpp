6c32cd7ed1d99d91e7e5842c08e3ad2d7af7b8fc
RyanArif <ryan@ryanarif.com>
2022-12-08 21:50:46 -0600

    Factorials

--- assignments/unit7/Recursion_Arif.cpp
+++ assignments/unit7/Recursion_Arif.cpp
@@ -8,18 +8,46 @@ Recursion: to repeat the same steps over and over again. In computer science, it
 */
 
 #include<iostream> //cout
+#include "Input_Validation_Extended.h"
 
 using namespace std;
 
 void towerOfHanoi(int, char, char, char);
+int factorialR(int);
+int factorialMultiplication(int);
+int getPosInt(int &);
 
 int main()
 {
+  int userInt = 0;
+
+  cout << "TOWER OF HANOI" << endl;
+  cout << "How many discs for your tower: ";
+  userInt = getPosInt(userInt);
+
   //solve the towers of hanoi problem with a 5 disc tower.
   //moves everything from tower A, to tower B, using tower C as the temporary tower stacking place.
-  towerOfHanoi(5, 'a', 'b', 'c');
+  towerOfHanoi(userInt, 'a', 'b', 'c');
+
+  cout << "Factorials!" << endl;
+  cout << "What number would you like to know the factorial for: ";
+  userInt = getPosInt(userInt);
+
+  cout << userInt << "! = " << factorialR(userInt) << endl;
 
   return 0;
+}
+
+int getPosInt(int & toValidate)
+{
+  toValidate = validateInt(toValidate); //get validated input
+  if(toValidate < 0)
+  {
+    cout << "ERROR: Enter a positive integer." << endl;
+    toValidate = getPosInt(toValidate);
+  }
+
+  return toValidate;
 }
 
 //moves some disks around some rods to solve the tower of hanoi problem.
@@ -40,5 +68,27 @@ void towerOfHanoi(int numDisks, char fromTower, char toTower, char otherTower)
   cout << "Moved disk " << numDisks << " from tower " << fromTower << " to tower " << toTower << "." << endl;
   //then unpack the auxilary tower and move it to the other tower that the stuff needs to be on.
   towerOfHanoi(numDisks-1, otherTower, toTower, fromTower);
+}
+
+//returns the result of the factorial of a given integer
+//example: 5! = 5 * 4 * 3 * 2 * 1 = 120
+int factorialR(int num)
+{
+  //edge case: 0! = 1
+  if(num == 0)
+  {
+    return 1;
+  }
+
+  if(num == 1) //if we reach the end of the factorial,
+  {
+    return 1; //return 1
+  }else{
+    return(num * factorialR(num-1));
+    //factorialR(5)
+    //5 * factorialR(4)
+    //5 * (4 * factorialR(3))
+    //5 * (4 * 3 * factorialR(2)....
+  }
 }
 
